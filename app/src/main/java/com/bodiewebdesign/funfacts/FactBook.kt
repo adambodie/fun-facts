09d2d19ad338465e8d816187db658557d2726f64
Adam Bodie <adam.bodie@yahoo.com>
2017-11-06 19:05:11 -0800

    Added DarkActionBar theme, updated styling

--- app/src/main/java/com/bodiewebdesign/funfacts/FactBook.kt
+++ app/src/main/java/com/bodiewebdesign/funfacts/FactBook.kt
@@ -5,16 +5,16 @@ import java.util.*
 class FactBook {
     // Properties - characteristics of the object
     val facts = arrayOf(
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built.")
+            "Black Cat Appreciation Day is August 17.",
+            "In January 2005, Richard Hamilton of the Detroit Pistons became the first player in NBA history to lead his team in scoring in a game without making a field goal, scoring 14 points.",
+            "The first known indoor billiard table was owned by King Louis XI of France.",
+            "News anchor Peter Jennings only lasted in school until the 10th grade.",
+            "The decision as to which of the Wright Brothers would make the first powered flight was decided by a coin flip.  Wilbur won.",
+            "Under most favorable circumstances, total solar eclipses can last for more than seven minutes.",
+            "The Sahara Desert covers 3.5 million square miles, about 31 percent of the continent of Africa.",
+            "The association between baseball and hot dogs began as early as 1893 with a German immigrant who owned the St. Louis Browns (now the St. Louis Cardinals) and an amusement park.",
+            "The music group Collective Soul is named from a phrase used in Ayn Rand's The Fountainhead.",
+            "Sunday Funday, the last game released in North America for the 8-bit Nintendo Entertainment System, is about a skateboarding hero who's late for Sunday School.")
 
     // Methods - actions the object can take
     fun getFact(): String {
