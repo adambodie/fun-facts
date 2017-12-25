a7a9c30d439888d39be03710948468279f761725
Adam Bodie <adam.bodie@yahoo.com>
2017-12-25 15:09:05 -0800

    Added Firebase dependency, code to retrieve facts from database

---
+++ app/src/main/java/com/bodiewebdesign/funfacts/Fact.kt
@@ -0,0 +1,9 @@
+package com.bodiewebdesign.funfacts
+
+class Fact {
+    var fact:String = ""
+    constructor() {}
+    constructor(fact:String) {
+        var fact = this.fact
+    }
+}
--- app/src/main/java/com/bodiewebdesign/funfacts/FunFactsActivity.kt
+++ app/src/main/java/com/bodiewebdesign/funfacts/FunFactsActivity.kt
@@ -9,12 +9,12 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.util.*
 
 
 class FunFactsActivity : AppCompatActivity() {
     private val TAG = FunFactsActivity::class.java.simpleName
 
-    private val factBook = FactBook()
     private val colorWheel = ColorWheel()
 
     // Declare our View variables
@@ -25,38 +25,30 @@ class FunFactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fun_facts)
-        /*val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("facts").child("fact")
-        //Log.d(TAG, "Test --- myRef is " + myRef)
-        //Log.d(TAG, "Test --- myRef.child() is " + myRef.child("1"))
-        myRef.addListenerForSingleValueEvent(object: ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                //val value = dataSnapshot.getValue(String::class.java)
-                //Log.d(TAG, "Test --- value is: " + value)
-                for (ds in dataSnapshot.getChildren()) {
-                    val point = ds.getKey()
-                    Log.d("TAG", "Test --- " + point)
-                }
-            }
-            override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
-                Log.w(TAG, "Test --- Failed to read value.", error.toException())
-            }
-        })*/
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference().child("facts")
         // Assign the Views from the layout file to the corresponding variables
         factTextView = findViewById<TextView>(R.id.factTextView) as TextView
         showFactButton = findViewById<Button>(R.id.showFactButton) as Button
         relativeLayout = findViewById<RelativeLayout>(R.id.relativeLayout) as RelativeLayout
         showFactButton!!.setOnClickListener{
-            val fact = factBook.getFact()
+            val randomGenerator = Random()
+            val randomNumber = randomGenerator.nextInt(20)
+            myRef.child(randomNumber.toString()).addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    var randomFact = dataSnapshot.getValue(Fact::class.java)
+                    var value = randomFact!!.fact
+                    factTextView!!.text = value
+                    factTextView!!.setScaleX(0F)
+                    factTextView!!.setScaleY(0F)
+                    factTextView!!.animate().setDuration(1500L).scaleX(1.0F).scaleY(1.0F).start()
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                    Log.w(TAG, "Test --- Failed to read value.", error.toException())
+                }
+            })
             val color = colorWheel.getColor()
-            // Update the screen with our new fact
-            factTextView!!.text = fact
-            factTextView!!.setScaleX(0F)
-            factTextView!!.setScaleY(0F)
-            factTextView!!.animate().setDuration(1500L).scaleX(1.0F).scaleY(1.0F).start()
             relativeLayout!!.setBackgroundColor(color)
             showFactButton!!.setTextColor(color)
         }
