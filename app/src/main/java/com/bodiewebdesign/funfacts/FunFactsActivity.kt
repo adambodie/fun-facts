5daf38fbae86aaaace92a4bda242517be37b3fbb
Adam Bodie <adam.bodie@yahoo.com>
2018-01-02 18:30:26 -0800

    Updated syntax to utilize Kotlin features

--- app/src/main/java/com/bodiewebdesign/funfacts/FunFactsActivity.kt
+++ app/src/main/java/com/bodiewebdesign/funfacts/FunFactsActivity.kt
@@ -1,4 +1,5 @@
 package com.bodiewebdesign.funfacts
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,7 +17,6 @@ class FunFactsActivity : AppCompatActivity() {
     private val TAG = FunFactsActivity::class.java.simpleName
 
     private val colorWheel = ColorWheel()
-
     // Declare our View variables
     private var factTextView: TextView? = null
     private var showFactButton: Button? = null
@@ -26,26 +26,29 @@ class FunFactsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fun_facts)
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference().child("facts")
+        val myRef = database.reference.child("facts")
         // Assign the Views from the layout file to the corresponding variables
         factTextView = findViewById<TextView>(R.id.factTextView) as TextView
         showFactButton = findViewById<Button>(R.id.showFactButton) as Button
         relativeLayout = findViewById<RelativeLayout>(R.id.relativeLayout) as RelativeLayout
+
         showFactButton!!.setOnClickListener{
             val randomGenerator = Random()
             val randomNumber = randomGenerator.nextInt(20)
             myRef.child(randomNumber.toString()).addValueEventListener(object: ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    var randomFact = dataSnapshot.getValue(Fact::class.java)
-                    var value = randomFact!!.fact
+                    val randomFact = dataSnapshot.getValue(Fact::class.java)
+                    val value = randomFact!!.fact
                     factTextView!!.text = value
-                    factTextView!!.setScaleX(0F)
-                    factTextView!!.setScaleY(0F)
-                    factTextView!!.animate().setDuration(1500L).scaleX(1.0F).scaleY(1.0F).start()
+                    factTextView!!.alpha = 0F
+                    factTextView!!.scaleX = 0F
+                    factTextView!!.scaleY = 0F
+                    factTextView!!.animate().setDuration(1500L).scaleX(1.0F).scaleY(1.0F).alphaBy(1.0F)
+                            .start()
                 }
                 override fun onCancelled(error: DatabaseError) {
                     // Failed to read value
-                    Log.w(TAG, "Test --- Failed to read value.", error.toException())
+                    Log.w(TAG, "Failed to read value.", error.toException())
                 }
             })
             val color = colorWheel.getColor()
